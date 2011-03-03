81dae5097a225a54257c6996c0a345f33b05138c
Mark C. Prins <mprins@users.sf.net>
2011-03-03 23:02:14 +0000

    update docs

--- action.php
+++ action.php
@@ -14,7 +14,7 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
- 
+
 /**
  * DokuWiki Plugin geotag (Action Component)
  *
@@ -30,43 +30,63 @@ if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once DOKU_PLUGIN.'action.php';
 
 class action_plugin_geotag extends DokuWiki_Action_Plugin {
-
-    public function register(Doku_Event_Handler &$controller) {
+	
+	/**
+	 * Register for events.
+	 *
+	 * @param $controller DokuWiki's event controller object. Also available as global $EVENT_HANDLER
+	 */
+	public function register(Doku_Event_Handler &$controller) {
 		$controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this, 'handle_metaheader_output');
-		$controller->register_hook('IO_WIKIPAGE_WRITE', 'BEFORE', $this, 'ping', array());
-    }
+		$controller->register_hook('IO_WIKIPAGE_WRITE', 'BEFORE', $this, 'ping_geourl', array());
+	}
 
+	/**
+	 * retrieve metadata and add to the head of the page using appropriate meta tags.
+	 * @param Doku_Event $event the DokuWiki event
+	 * @param unknown_type $param
+	 */
 	public function handle_metaheader_output(Doku_Event &$event, $param) {
 		global $ID;
+
+		$title = p_get_metadata($ID,'title',true);
 		$geotags = p_get_metadata($ID,'geo',true);
 		$region=$geotags['region'];
 		$lat=$geotags['lat'];
 		$lon=$geotags['lon'];
 		$country=$geotags['country'];
 		$placename=$geotags['placename'];
-		
+
 		if (!empty($region)) {$event->data['meta'][] = array('name' => 'geo.region','content' => $region,);}
 		if (!empty($placename)) {$event->data['meta'][] = array('name' => 'geo.placename','content' => $placename,);}
 		if (!(empty($lat)&&empty($lon))) {$event->data['meta'][] = array('name' => 'geo.position','content' => $lat.';'.$lon,);}
 		if (!empty($country)) {$event->data['meta'][] = array('name' => 'geo.country','content' => $country,);}
-		if (!(empty($lat)&&empty($lon))) {$event->data['meta'][] = array('name' => 'ICBM','content' => $lat.', '.$lon,);}
-    }
-	
-	function ping(&$event, $param) {
-	    if (!$this->getConf('geotag_pinggeourl')) return false;     // config says don't ping
-        if ($event->data[3]) return false;                   // old revision saved
-        if (@file_exists($event->data[0][0])) return false;  // file not new
-        if (!$event->data[0][1]) return false;               // file is empty
+		if (!(empty($lat)&&empty($lon))) {$event->data['meta'][] = array('name' => "ICBM",'content' => $lat.', '.$lon,);}
+		// icbm is useless without a dc.title, so we copy that from title
+		if (!(empty($title))) {$event->data['meta'][] = array('name' => "DC.title",'content' => $title);}
+	}
 
-        global $conf;
+	/**
+	 * Ping the geourl webservice with th eurl of the for indexing.
+	 * @param Doku_Event $event the DokuWiki event
+	 * @param unknown_type $param
+	 */
+	function ping_geourl(Doku_Event &$event, $param) {
+		if (!$this->getConf('geotag_pinggeourl')) return false; // config says don't ping
+		if ($event->data[3]) return false;                   // old revision saved
+		if (@file_exists($event->data[0][0])) return false;  // file not new
+		if (!$event->data[0][1]) return false;               // file is empty
+		if (p_get_metadata($ID,'geo',true)) return false; // no geo metadata available
 
-        $request = 'p='.DOKU_URL;
-        $url = 'http://geourl.org/ping/';
-        $header[] = 'Host: geourl.org';
-        $header[] = 'Content-type: text/html';
-        $header[] = 'Content-length: '.strlen($request);
+		global $conf;
 
-        $http = new DokuHTTPClient();
-        return $http->get($url, $request);
+		$request = 'p='.DOKU_URL;
+		$url = 'http://geourl.org/ping/';
+		$header[] = 'Host: geourl.org';
+		$header[] = 'Content-type: text/html';
+		$header[] = 'Content-length: '.strlen($request);
+
+		$http = new DokuHTTPClient();
+		return $http->get($url, $request);
 	}
 }
