2b579309fed27c4c706243b75b39813e5b6dda9c
Mark C. Prins <mprins@users.sf.net>
2013-11-22 11:15:06 +0000

    add support for altitude Issue number 36

--- action.php
+++ action.php
@@ -14,119 +14,160 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
-if (!defined('DOKU_INC')) die();
+if (! defined ( 'DOKU_INC' ))
+	die ();
 
-if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-require_once(DOKU_PLUGIN.'action.php');
+if (! defined ( 'DOKU_PLUGIN' ))
+	define ( 'DOKU_PLUGIN', DOKU_INC . 'lib/plugins/' );
+require_once (DOKU_PLUGIN . 'action.php');
 
 /**
  * DokuWiki Plugin geotag (Action Component).
  *
  * @license BSD license
- * @author  Mark C. Prins <mprins@users.sf.net>
+ * @author Mark C. Prins <mprins@users.sf.net>
  */
 class action_plugin_geotag extends DokuWiki_Action_Plugin {
-
+	
 	/**
 	 * Register for events.
 	 *
-	 * @param $controller DokuWiki's event controller object. Also available as global $EVENT_HANDLER
+	 * @param Doku_Event_Handler $controller
+	 *        	DokuWiki's event controller object. Also available as global $EVENT_HANDLER
 	 */
-	public function register(Doku_Event_Handler &$controller) {
-		$controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this, 'handle_metaheader_output');
-		$controller->register_hook('IO_WIKIPAGE_WRITE', 'BEFORE', $this, 'ping_geourl', array());
-		if($this->getConf('toolbar_icon')) {
-			$controller->register_hook('TOOLBAR_DEFINE', 'AFTER', $this, 'insert_button', array ());
+	public function register(Doku_Event_Handler $controller) {
+		$controller->register_hook ( 'TPL_METAHEADER_OUTPUT', 'BEFORE', $this, 'handle_metaheader_output' );
+		$controller->register_hook ( 'IO_WIKIPAGE_WRITE', 'BEFORE', $this, 'ping_geourl', array () );
+		if ($this->getConf ( 'toolbar_icon' )) {
+			$controller->register_hook ( 'TOOLBAR_DEFINE', 'AFTER', $this, 'insert_button', array () );
 		}
 	}
-
+	
 	/**
-	 * retrieve metadata and add to the head of the page using appropriate meta tags.
-	 * @param Doku_Event $event the DokuWiki event. $event->data is a two-dimensional
-	 * 	array of all meta headers. The keys are meta, link and script.
-	 * @param unknown_type $param
-	 * 
-	 * @see  http://www.dokuwiki.org/devel:event:tpl_metaheader_output
+	 * Retrieve metadata and add to the head of the page using appropriate meta tags.
+	 *
+	 * @param Doku_Event $event
+	 *        	the DokuWiki event. $event->data is a two-dimensional
+	 *        	array of all meta headers. The keys are meta, link and script.
+	 * @param unknown_type $param        	
+	 *
+	 * @see http://www.dokuwiki.org/devel:event:tpl_metaheader_output
 	 */
 	public function handle_metaheader_output(Doku_Event &$event, $param) {
 		global $ID;
-		$title = p_get_metadata($ID,'title',true);
-		$geotags = p_get_metadata($ID,'geo',true);
-		$region=$geotags['region'];
-		$lat=$geotags['lat'];
-		$lon=$geotags['lon'];
-		$country=$geotags['country'];
-		$placename=$geotags['placename'];
-		$geohash=$geotags['geohash'];
-
-		if (!empty($region)) {
-			$event->data['meta'][] = array('name' => 'geo.region','content' => $region,);
+		$title = p_get_metadata ( $ID, 'title', true );
+		$geotags = p_get_metadata ( $ID, 'geo', true );
+		$region = $geotags ['region'];
+		$lat = $geotags ['lat'];
+		$lon = $geotags ['lon'];
+		$alt = $geotags ['alt'];
+		$country = $geotags ['country'];
+		$placename = $geotags ['placename'];
+		$geohash = $geotags ['geohash'];
+		
+		if (! empty ( $region )) {
+			$event->data ['meta'] [] = array (
+					'name' => 'geo.region',
+					'content' => $region 
+			);
 		}
-		if (!empty($placename)) {
-			$event->data['meta'][] = array('name' => 'geo.placename','content' => $placename,);
+		if (! empty ( $placename )) {
+			$event->data ['meta'] [] = array (
+					'name' => 'geo.placename',
+					'content' => $placename 
+			);
 		}
-		if (!(empty($lat)&&empty($lon))) {
-			$event->data['meta'][] = array('name' => 'geo.position','content' => $lat.';'.$lon,);
-		}
-		if (!empty($country)) {
-			$event->data['meta'][] = array('name' => 'geo.country','content' => $country,);
-		}
-		if (!(empty($lat)&&empty($lon))) {
-			$event->data['meta'][] = array('name' => "ICBM",'content' => $lat.', '.$lon,);
-			// icbm is generally useless without a dc.title, so we copy that from title unless it's empty
-			if (!(empty($title))) {
-				$event->data['meta'][] = array('name' => "DC.title",'content' => $title);
+		if (! (empty ( $lat ) && empty ( $lon ))) {
+			$event->data ['meta'] [] = array (
+					'name' => 'geo.position',
+					'content' => $lat . ';' . $lon 
+			);
+			if (! empty ( $alt )) {
+				$event->data ['meta'] [] = array (
+						'name' => 'geo.position',
+						'content' => $lat . ';' . $lon . ';' . $alt 
+				);
 			}
 		}
-		if (!empty($geohash)) {
-			$event->data['meta'][] = array('name' => 'geo.geohash','content' => $geohash,);
+		if (! empty ( $country )) {
+			$event->data ['meta'] [] = array (
+					'name' => 'geo.country',
+					'content' => $country 
+			);
+		}
+		if (! (empty ( $lat ) && empty ( $lon ))) {
+			$event->data ['meta'] [] = array (
+					'name' => "ICBM",
+					'content' => $lat . ', ' . $lon 
+			);
+			// icbm is generally useless without a dc.title,
+			// so we copy that from title unless it's empty
+			if (! (empty ( $title ))) {
+				$event->data ['meta'] [] = array (
+						'name' => "DC.title",
+						'content' => $title 
+				);
+			}
+		}
+		if (! empty ( $geohash )) {
+			$event->data ['meta'] [] = array (
+					'name' => 'geo.geohash',
+					'content' => $geohash 
+			);
 		}
 	}
-
+	
 	/**
 	 * Ping the geourl webservice with the url of the for indexing, only if the page is new.
-	 * 
-	 * @param Doku_Event $event the DokuWiki event
-	 * @param mixed $param not used
+	 *
+	 * @param Doku_Event $event
+	 *        	the DokuWiki event
+	 * @param mixed $param
+	 *        	not used
 	 */
 	function ping_geourl(Doku_Event &$event, $param) {
 		global $ID;
 		/*
-		 * see: http://www.dokuwiki.org/devel:event:io_wikipage_write
-		 * event data:
-		 * $data[0] – The raw arguments for io_saveFile as an array. Do not change file path.
-		 * $data[0][0] – the file path.
-		 * $data[0][1] – the content to be saved, and may be modified.
-		 * $data[1] – ns: The colon separated namespace path minus the trailing page name. (false if root ns)
-		 * $data[2] – page_name: The wiki page name.
+		 * see: http://www.dokuwiki.org/devel:event:io_wikipage_write event data: 
+		 * $data[0] – The raw arguments for io_saveFile as an array. Do not change file path. 
+		 * $data[0][0] – the file path. 
+		 * $data[0][1] – the content to be saved, and may be modified. 
+		 * $data[1] – ns: The colon separated namespace path minus the trailing page name. (false if root ns) 
+		 * $data[2] – page_name: The wiki page name. 
 		 * $data[3] – rev: The page revision, false for current wiki pages.
 		 */
-		if (!$this->getConf('geotag_pinggeourl')) return false; // config says don't ping
-		if ($event->data[3]) return false;                      // old revision saved
-		if (@file_exists($event->data[0][0])) return false;     // file not new
-		if (!$event->data[0][1]) return false;                  // file is empty
-		if (p_get_metadata($ID,'geo',true)) return false;       // no geo metadata available, ping is useless
-
-		$url = 'http://geourl.org/ping/?p='.wl($ID,'',true);
-		$http = new DokuHTTPClient();
-		$result = $http->get($url);
-		dbglog($result,"GeoURL Ping response for $url");
+		if (! $this->getConf ( 'geotag_pinggeourl' ))
+			return false; // config says don't ping
+		if ($event->data [3])
+			return false; // old revision saved
+		if (@file_exists ( $event->data [0] [0] ))
+			return false; // file not new
+		if (! $event->data [0] [1])
+			return false; // file is empty
+		if (p_get_metadata ( $ID, 'geo', true ))
+			return false; // no geo metadata available, ping is useless
+		
+		$url = 'http://geourl.org/ping/?p=' . wl ( $ID, '', true );
+		$http = new DokuHTTPClient ();
+		$result = $http->get ( $url );
+		dbglog ( $result, "GeoURL Ping response for $url" );
 		return $result;
 	}
-
+	
 	/**
 	 * Inserts the toolbar button.
 	 *
-	 * @param Doku_Event $event the DokuWiki event
+	 * @param Doku_Event $event
+	 *        	the DokuWiki event
 	 */
 	function insert_button(Doku_Event &$event, $param) {
-		$event->data[] = array (
+		$event->data [] = array (
 				'type' => 'format',
-				'title' => $this->getLang('toolbar_desc'),
+				'title' => $this->getLang ( 'toolbar_desc' ),
 				'icon' => '../../plugins/geotag/images/geotag.png',
 				'open' => '{{geotag>lat:',
 				'sample' => '52.2345',
-				'close' => ', lon: 7.521, placename: , country: , region: }}'
+				'close' => ', lon: 7.521, alt: , placename: , country: , region: }}' 
 		);
 	}
 }
--- syntax/geotag.php
+++ syntax/geotag.php
@@ -14,130 +14,149 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
+if (! defined ( 'DOKU_INC' ))
+	die ();
 
+if (! defined ( 'DOKU_PLUGIN' ))
+	define ( 'DOKU_PLUGIN', DOKU_INC . 'lib/plugins/' );
 
-if (!defined('DOKU_INC')) die();
-
-if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-
-require_once(DOKU_PLUGIN.'syntax.php');
+require_once (DOKU_PLUGIN . 'syntax.php');
 /**
- * DokuWiki Plugin geotag (Syntax Component). 
+ * DokuWiki Plugin geotag (Syntax Component).
+ *
  * Handles the rendering part of the geotag plugin.
  *
  * @license BSD license
- * @author  Mark C. Prins <mprins@users.sf.net>
+ * @author Mark C. Prins <mprins@users.sf.net>
  */
 class syntax_plugin_geotag_geotag extends DokuWiki_Syntax_Plugin {
 	/**
 	 * (non-PHPdoc)
+	 * 
 	 * @see DokuWiki_Syntax_Plugin::getType()
 	 */
-	public function getType() { return 'substition'; }
-
+	public function getType() {
+		return 'substition';
+	}
+	
 	/**
 	 * (non-PHPdoc)
+	 * 
 	 * @see DokuWiki_Syntax_Plugin::getPType()
 	 */
-	public function getPType() { return 'block'; }
-
+	public function getPType() {
+		return 'block';
+	}
+	
 	/**
 	 * (non-PHPdoc)
+	 * 
 	 * @see Doku_Parser_Mode::getSort()
 	 */
-	public function getSort() { return 305; }
-
+	public function getSort() {
+		return 305;
+	}
+	
 	/**
 	 * (non-PHPdoc)
+	 * 
 	 * @see Doku_Parser_Mode::connectTo()
 	 */
 	public function connectTo($mode) {
-		$this->Lexer->addSpecialPattern('\{\{geotag>.*?\}\}',$mode,'plugin_geotag_geotag');
+		$this->Lexer->addSpecialPattern ( '\{\{geotag>.*?\}\}', $mode, 'plugin_geotag_geotag' );
 	}
-
+	
 	/**
 	 * (non-PHPdoc)
+	 * 
 	 * @see DokuWiki_Syntax_Plugin::handle()
 	 */
-	public function handle($match, $state, $pos, &$handler){
-		$tags = trim(substr($match, 9, -2));
+	public function handle($match, $state, $pos, Doku_Handler &$handler) {
+		$tags = trim ( substr ( $match, 9, - 2 ) );
 		// parse geotag content
-		preg_match("(lat[:|=]\d*\.\d*)",$tags,$lat);
-		preg_match("(lon[:|=]\d*\.\d*)",$tags,$lon);
-		preg_match("(region[:|=][a-zA-Z\s\w'-]*)",$tags,$region);
-		preg_match("(placename[:|=][a-zA-Z\s\w'-]*)",$tags,$placename);
-		preg_match("(country[:|=][a-zA-Z\s\w'-]*)",$tags,$country);
-		preg_match("(hide|unhide)",$tags,$hide);
-
-		$showlocation=$this->getConf('geotag_location_prefix');
-		if ($this->getConf('geotag_showlocation')) {
-			$showlocation=trim(substr($placename[0],10));
-			if (strlen($showlocation)>0) {
-				$showlocation .=': ';
+		preg_match ( "(lat[:|=]\d*\.\d*)", $tags, $lat );
+		preg_match ( "(lon[:|=]\d*\.\d*)", $tags, $lon );
+		preg_match ( "(alt[:|=]\d*\.?\d*)", $tags, $alt );
+		preg_match ( "(region[:|=][a-zA-Z\s\w'-]*)", $tags, $region );
+		preg_match ( "(placename[:|=][a-zA-Z\s\w'-]*)", $tags, $placename );
+		preg_match ( "(country[:|=][a-zA-Z\s\w'-]*)", $tags, $country );
+		preg_match ( "(hide|unhide)", $tags, $hide );
+		
+		$showlocation = $this->getConf ( 'geotag_location_prefix' );
+		if ($this->getConf ( 'geotag_showlocation' )) {
+			$showlocation = trim ( substr ( $placename [0], 10 ) );
+			if (strlen ( $showlocation ) > 0) {
+				$showlocation .= ': ';
 			}
 		}
 		// read config for system setting
-		$style='';
-		if ($this->getConf('geotag_hide')) {
-			$style=' style="display: none;"';
+		$style = '';
+		if ($this->getConf ( 'geotag_hide' )) {
+			$style = ' style="display: none;"';
 		}
 		// override config for the current tag
-		if (trim($hide[0])=='hide'){
-			$style=' style="display: none;"';
-		} elseif(trim($hide[0])=='unhide'){
-			$style='';
+		if (trim ( $hide [0] ) == 'hide') {
+			$style = ' style="display: none;"';
+		} elseif (trim ( $hide [0] ) == 'unhide') {
+			$style = '';
 		}
-
-		$data = array(
-			trim(substr($lat[0],4)),
-			trim(substr($lon[0],4)),
-			$this->_geohash(substr($lat[0],4), substr($lon[0],4)),
-			trim(substr($region[0],7)),
-			trim(substr($placename[0],10)),
-			trim(substr($country[0],8)),
-			$showlocation,
-			$style,
+		
+		$data = array (
+				trim ( substr ( $lat [0], 4 ) ),
+				trim ( substr ( $lon [0], 4 ) ),
+				trim ( substr ( $alt [0], 4 ) ),
+				$this->_geohash ( substr ( $lat [0], 4 ), substr ( $lon [0], 4 ) ),
+				trim ( substr ( $region [0], 7 ) ),
+				trim ( substr ( $placename [0], 10 ) ),
+				trim ( substr ( $country [0], 8 ) ),
+				$showlocation,
+				$style 
 		);
 		return $data;
 	}
-
+	
 	/**
 	 * (non-PHPdoc)
+	 * 
 	 * @see DokuWiki_Syntax_Plugin::render()
 	 */
-	public function render($mode, &$renderer, $data) {
-		if ($data === false) return false;
-		list ($lat, $lon, $geohash, $region, $placename, $country, $showlocation, $style) = $data;
+	public function render($mode, Doku_Renderer &$renderer, $data) {
+		if ($data === false)
+			return false;
+		list ( $lat, $lon, $alt, $geohash, $region, $placename, $country, $showlocation, $style ) = $data;
 		if ($mode == 'xhtml') {
-			if ($this->getConf('geotag_prevent_microformat_render')) {
+			if ($this->getConf ( 'geotag_prevent_microformat_render' )) {
 				// config says no microformat rendering
 				return true;
 			}
-			// render geotag microformat
-			$renderer->doc .= '<span class="geotagPrint">'.
-					$this->getLang('geotag_desc').'</span><div class="geo"'.
-					$style.' title="'.$this->getLang('geotag_desc').$placename.'">'.
-					$showlocation.'<span class="latitude">'.
-					$lat.'</span>;<span class="longitude">'.
-					$lon.'</span></div>'.DOKU_LF;
+			if (! empty ( $alt ))
+				$alt = ', <span class="altitude">' . $alt . 'm</span>';
+				// render geotag microformat
+			$renderer->doc .= '<span class="geotagPrint">' . $this->getLang ( 'geotag_desc' ) . '</span>';
+			$renderer->doc .= '<div class="geo"' . $style . ' title="' . 
+				$this->getLang ( 'geotag_desc' ) . $placename . '">' . $showlocation . 
+				'<span class="latitude">' . $lat . '</span>;<span class="longitude">' . $lon . '</span>' . $alt . '</div>' . DOKU_LF;
 			return true;
 		} elseif ($mode == 'metadata') {
 			// render metadata (action plugin will put it in the page head)
-			$renderer->meta['geo']['lat'] = $lat;
-			$renderer->meta['geo']['lon'] = $lon;
-			$renderer->meta['geo']['placename'] = $placename;
-			$renderer->meta['geo']['region'] = $region;
-			$renderer->meta['geo']['country'] = $country;
-			$renderer->meta['geo']['geohash'] = $geohash;
+			$renderer->meta ['geo'] ['lat'] = $lat;
+			$renderer->meta ['geo'] ['lon'] = $lon;
+			$renderer->meta ['geo'] ['placename'] = $placename;
+			$renderer->meta ['geo'] ['region'] = $region;
+			$renderer->meta ['geo'] ['country'] = $country;
+			$renderer->meta ['geo'] ['geohash'] = $geohash;
+			$renderer->meta ['geo'] ['alt'] = $alt;
 			return true;
-		} elseif ($mode=='odt'){
-			$renderer->p_open();
-			$renderer->_odtAddImage(DOKU_PLUGIN.'geotag/images/geotag.png',null, null, 'left','');
-			$renderer->doc .= '<text:span>'.$this->getLang('geotag_desc').' '.$placename.': </text:span>';
-			$renderer->monospace_open();
-			$renderer->doc .= $lat.';'.$lon;
-			$renderer->monospace_close();
-			$renderer->p_close();
+		} elseif ($mode == 'odt') {
+			if (! empty ( $alt ))
+				$alt = ', ' . $alt . 'm';
+			$renderer->p_open ();
+			$renderer->_odtAddImage ( DOKU_PLUGIN . 'geotag/images/geotag.png', null, null, 'left', '' );
+			$renderer->doc .= '<text:span>' . $this->getLang ( 'geotag_desc' ) . ' ' . $placename . ': </text:span>';
+			$renderer->monospace_open ();
+			$renderer->doc .= $lat . 'º;' . $lon . 'º' . $alt;
+			$renderer->monospace_close ();
+			$renderer->p_close ();
 			return true;
 		}
 		return false;
@@ -145,19 +164,19 @@ class syntax_plugin_geotag_geotag extends DokuWiki_Syntax_Plugin {
 	
 	/**
 	 * Calculate the geohash for this lat/lon pair.
-	 * 
-	 * @param float $lat
-	 * @param float $lon
+	 *
+	 * @param float $lat        	
+	 * @param float $lon        	
 	 */
-	private function _geohash($lat, $lon){
-		if (!$geophp = &plugin_load('helper', 'geophp')){
-			dbglog($geophp,'syntax_plugin_geotag_geotag::_geohash: geophp plugin is not available.');
+	private function _geohash($lat, $lon) {
+		if (! $geophp = &plugin_load ( 'helper', 'geophp' )) {
+			dbglog ( $geophp, 'syntax_plugin_geotag_geotag::_geohash: geophp plugin is not available.' );
 			return "";
 		}
-		$_lat = floatval($lat);
-		$_lon = floatval($lon);
-		$geometry = new Point($_lon,$_lat);
-		//dbglog($geometry, 'geometry to calculate geohash from..');
-		return $geometry->out('geohash');
+		$_lat = floatval ( $lat );
+		$_lon = floatval ( $lon );
+		$geometry = new Point ( $_lon, $_lat );
+		// dbglog($geometry, 'geometry to calculate geohash from..');
+		return $geometry->out ( 'geohash' );
 	}
 }
