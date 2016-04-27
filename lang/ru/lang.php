2293e8926da1f776e39a7343588710dcd7db9b69
Mark C. Prins <mprins@users.sf.net>
2016-04-27 07:35:24 +0000

    Add Russian translation provided by Козлов Алексей

---
+++ lang/ru/lang.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Copyright (c) 2016 Mark C. Prins <mprins@users.sf.net>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * Russian language file for geotag plugin.
+ *
+ * @license BSD license
+ * @author  Aleksej Kozlov <kozlov@ritmsoft.ru>
+ */
+$lang['geotag_desc'] = 'Геотег (местоположение):';
+$lang['toolbar_desc'] = 'Вставить геотег';
+$lang['findnearby'] = 'Искать поблизости';
---
+++ lang/ru/settings.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * Copyright (c) 2016 Mark C. Prins <mprins@users.sf.net>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * Russian language file for geotag plugin.
+ *
+ * @license BSD license
+ * @author  Aleksej Kozlov <kozlov@ritmsoft.ru>
+ */
+$lang['geotag_pinggeourl'] = 'Уведомить GeoURL.org о необходимости индексирования';
+$lang['geotag_location_prefix'] = 'Текст, по умолчанию показываемый перед геотегом';
+$lang['geotag_showlocation'] = 'Показывать название местоположения в геотеге (вместо текста по умолчанию)';
+$lang['geotag_hide'] = 'Прятать геотег (средствами css)';
+$lang['geotag_prevent_microformat_render'] = 'Запретить рендеринг микроформата геотега';
+$lang['toolbar_icon'] = 'Показывать иконку';
+$lang['geotag_showsearch'] = 'Геотег является ссылкой на страницу поиска поблизости (нужен плагин spatialhelper)';
+$lang['displayformat'] = 'Формат отображения координат: DD - градусы с дробной частью, DMS - градусы, минуты, секунды';
