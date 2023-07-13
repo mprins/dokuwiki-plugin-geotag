ba10d2a87a8d434b46e70293d84851e6b2082501
Mark Prins <1165786+mprins@users.noreply.github.com>
2023-07-13 17:23:56 +0200

    copyright updates

--- _test/syntax.test.php
+++ _test/syntax.test.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright (c) 2016-2018 Mark C. Prins <mprins@users.sf.net>
+ * Copyright (c) 2016 Mark C. Prins <mprins@users.sf.net>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
